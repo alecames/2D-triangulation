7d2846637817e5a679deb799996ffc9817735119
julellisg <julianellisg@gmail.com>
2023-03-10 01:43:32 -0500

    Slight code arrangements and added function to render points

--- 2d-triangulation/2dtriangulation.cpp
+++ 2d-triangulation/2dtriangulation.cpp
@@ -1,33 +1,29 @@
-
+#include <freeglut.h>
+#include <FreeImage.h>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <glut.h>
 #include <math.h>
 
+const int WIDTH = 800;
+const int HEIGHT = 800;
+
 // This program will preform 2D triangulation on a set of points
 
-struct Color { float r, g, b; };
-struct Cube { float x, y, z; };
-
-// formats the prints with the table edges
-void padprint(const char* str) {
-	int len = strlen(str);
-	int pad = 70 - len;
-	printf("| ");
-	printf("%s", str);
-	for (int i = 0; i < pad; i++)
-		printf(" ");
-	printf("|\n");
-	printf("|-----------------------------------------------------------------------|\r");
+// render the points to be triangulated to each other
+void drawPoints(int points) {
+	glPointSize(4);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < points; i++)
+		glVertex2i(rand() % WIDTH, rand() % HEIGHT);
+	glEnd();
 }
 
 // prints controls to terminal
 void showcmds() {
 	printf("|-----------------------------------------------------------------------|\n");
-	printf("| H: Help                   CUBE SCREENSAVER              ESC / Q: Quit |\n");
-	printf("|------------------------------ CONTROLS ------------------------------ |\n");
-	printf("| S: Slow mode | C: Change Colour | Z: Default | P: Pulse | R: RGB Mode |\n");
+	printf("|                          2D - TRIANGULATION             ESC / Q: Quit |\n");
 	printf("|-----------------------------------------------------------------------|\n");
 }
 
@@ -60,39 +56,35 @@ void keyboard(unsigned char key, int x, int y) {
 }
 
 void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(600, 1, 0.01, 1000);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+	// Set dot color to white
+	glColor3f(1.0, 1.0, 1.0);
+	drawPoints(5);
+
     glutSwapBuffers();
 }
 
-void idle(void) {
-    glutPostRedisplay();
-}
-
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1920, 1080);
-	glutInitWindowPosition(0, 0);
+	glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - WIDTH)/2, (glutGet(GLUT_SCREEN_HEIGHT) - HEIGHT)/2);	// makes the window appear in the center of the screen
     glutCreateWindow("2D Triangulation");
-	glutFullScreen();
-    glEnable(GL_DEPTH_TEST);
+
+	// initialize
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, WIDTH, 0, HEIGHT);	// creates drawable projection
 
 	// print controls
 	showcmds();
 
+	// flut functions
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
-    glutIdleFunc(idle);
-
-    // attempt to smooth edges
-    glEnable(GL_LINE_SMOOTH);
-    glEnable(GL_POLYGON_SMOOTH);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glutSetCursor(GLUT_CURSOR_NONE);
 
     glutMainLoop();
     return 0;

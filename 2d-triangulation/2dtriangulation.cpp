72fa04b542b1ababcb173b0132a4a0f77f3f269b
Alec Ames <dev@alecames.com>
2023-03-12 01:25:37 -0500

    asdkafab

--- 2d-triangulation/2dtriangulation.cpp
+++ 2d-triangulation/2dtriangulation.cpp
@@ -36,41 +36,32 @@ struct Edge {
 	bool operator<(const Edge& other) const { return length < other.length; }
 	bool operator>(const Edge& other) const { return length > other.length; }
 	bool operator==(const Edge& other) const { return p1 == other.p1 && p2 == other.p2; }
-	float slope() const { return (float)(p2.y - p1.y) / (float)(p2.x - p1.x); }
+	bool operator!=(const Edge& other) const { return p1 != other.p1 || p2 != other.p2; }
+	float slope() const {
+		if (p2.x == p1.x) { return std::numeric_limits<float>::infinity(); }
+		return (float)(p2.y - p1.y) / (float)(p2.x - p1.x);
+	}
+	bool connected(const Edge& other) const { return p1 == other.p1 || p1 == other.p2 || p2 == other.p1 || p2 == other.p2; }
 	bool intersects(Edge& other) {
-		// check if the two lines are parallel
-		if (slope() == other.slope())
-			return false;
-
-		// vertical lines
-		if (p1.x == p2.x || other.p1.x == other.p2.x)
-			return false;
-
-		// check if the two lines are horizontal
-		if (p1.y == p2.y || other.p1.y == other.p2.y)
-			return false;
+		if (slope() == other.slope()) return false;
+		if (p1.x == p2.x || other.p1.x == other.p2.x) return false;
+		if (p1.y == p2.y || other.p1.y == other.p2.y) return false;
 
 		// check if the two lines intersect
 		float x = ((p1.x * p2.y - p1.y * p2.x) * (other.p1.x - other.p2.x) - (p1.x - p2.x) * (other.p1.x * other.p2.y - other.p1.y * other.p2.x)) / ((p1.x - p2.x) * (other.p1.y - other.p2.y) - (p1.y - p2.y) * (other.p1.x - other.p2.x));
 		float y = ((p1.x * p2.y - p1.y * p2.x) * (other.p1.y - other.p2.y) - (p1.y - p2.y) * (other.p1.x * other.p2.y - other.p1.y * other.p2.x)) / ((p1.x - p2.x) * (other.p1.y - other.p2.y) - (p1.y - p2.y) * (other.p1.x - other.p2.x));
 
 		// check if the intersection point is within the bounds of the two lines
-		if (x < std::min(p1.x, p2.x) || x > std::max(p1.x, p2.x))
-			return false;
-		if (x < std::min(other.p1.x, other.p2.x) || x > std::max(other.p1.x, other.p2.x))
-			return false;
-		if (y < std::min(p1.y, p2.y) || y > std::max(p1.y, p2.y))
-			return false;
-		if (y < std::min(other.p1.y, other.p2.y) || y > std::max(other.p1.y, other.p2.y))
-			return false;
-
+		if (x < std::min(p1.x, p2.x) || x > std::max(p1.x, p2.x)) return false;
+		if (x < std::min(other.p1.x, other.p2.x) || x > std::max(other.p1.x, other.p2.x)) return false;
+		if (y < std::min(p1.y, p2.y) || y > std::max(p1.y, p2.y)) return false;
+		if (y < std::min(other.p1.y, other.p2.y) || y > std::max(other.p1.y, other.p2.y)) return false;
 		return true;
 	}
 
-	Point intersection(const Edge& other) const {
+	Point intersect(const Edge& other) const {
 		// check if the two lines are parallel
-		if (slope() == other.slope())
-			return Point{ -1, -1 };
+		if (slope() == other.slope()) return Point{ -1, -1 };
 
 		// vertical lines
 		if (p1.x == p2.x || other.p1.x == other.p2.x)
@@ -160,145 +151,45 @@ void drawPoints() {
 
 // b) naive triangulation algorithm
 void calcEdges() {
-	// calculate length, save to edge list;
-	int numEdges = 0;
-
-	// finding ALL possible edges
-	for (int i = 0; i < POINT_COUNT; i++) {
-		for (int j = i + 1; j < POINT_COUNT; j++) {
-			// calculate the distance between the two points
-			// Point 1
-			int x0 = P[i].x;
-			int y0 = P[i].y;
-			// Point 2
-			int x1 = P[j].x;
-			int y1 = P[j].y;
-			// Distance between 2 points (x0, y0) and (x1, y1): d = sqrt((x1-x0)^2 + (y1-y0)^2)
-			unsigned int length = sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2));
-
-			// printf("Edge #%i from i = P[%i] to j = P[%i]\tlength: %i\n", numEdges, i, j, length);
-
-			// b) save the triangles in a data structure
-			Edge e = { P[i], P[j], length };
-			EdgeList[numEdges] = e;
-			numEdges++;
+	int i = 0;
+	for (int j = 0; j < POINT_COUNT; j++) {
+		for (int k = j + 1; k < POINT_COUNT; k++) {
+			Edge e = { P[j], P[k] };
+			EdgeList[i] = e;
+			i++;
 		}
 	}
-
-	// b) count and print the number of edges created
-	printf("Edges: %i\n", numEdges);
-
-	// sort edge vector in order of least-greatest edge length (takes nlogn time)
-	std::sort(EdgeList.begin(), EdgeList.end());
-
-	// (FILTER) Find all edges with intersections and disregard them when inserting edges into TriList.
-	// This loop will compare every combination of edges and determine which ones have intersections with each other.
-	bool intersection;
-	TriEdge.clear();
-
-	for (int i = 0; i < numEdges; i++) {
-		Edge L1 = EdgeList[i];
-		intersection = false; // initially assume edge encounters no intersections
-
-		// compare L1 with each L2 in Tri Edge
-		for (Edge L2 : TriEdge) {
-			if (&L1 && &L2)
-				continue;
-
-			// Line Segment 1 (L1)
-			Point pointA = L1.p1;
-			int xa = pointA.x;
-			int ya = pointA.y;
-			Point pointB = L1.p2;
-			int xb = pointB.x;
-			int yb = pointB.y;
-
-			// Line Segment 2 (L2)
-			Point pointC = L2.p1;
-			int xc = pointC.x;
-			int yc = pointC.y;
-			Point pointD = L2.p2;
-			int xd = pointD.x;
-			int yd = pointD.y;
-
-			int D = (xb - xa) * (yd - yc) - (yb - ya) * (xd - xc);
-
-			if (D != 0) {
-				printf("D != 0\n");
-				int ta;
-				int tb;
-
-				// (i) L2 is not vertical
-				if ((xd - xc) != 0) {
-					printf("not vertical\n");
-					ta = ((xc - xa) * (yd - yc) - (yc - ya) * (xd - xc)) / D;
-					tb = (xa - xc + (xb - xa) * ta) / (xd - xc);
-
-					if ((ta >= 0 && ta <= 1) && (tb >= 0 && tb <= 1)) {
-						intersection = true;
-						break;
-					}
-				}
-				// (ii) L2 is vertical
-				else if ((xd - xc) == 0) {
-					printf("is vertical\n");
-					ta = (xc - xa) / (xb - xa);
-					tb = (ya + ta * (yb - ya) - yc) / (yd - yc);
-
-					if (ta >= 0 && ta <= 1 && tb >= 0 && tb <= 1) {
-						intersection = true;
-						break;
-					}
-				}
-			}
-			//	in D == 0, they might overlap, but we'll not need to deal with this
-		} // for-each
-
-		if (!intersection) { // if there is no intersection, then we can add it to TriEdge
-			TriEdge.insert(L1);
-		}
-
-	} // for
-
-	// b) print number of TriEdges
-	printf("TriEdges: %i\n", TriEdge.size());
-
-	printf("Tri-Length: ");
-	for (Edge e : TriEdge) {
-		printf("%i ", e.length);
-	}
-	printf("\n");
 }
 
 // c) triangle polygon extaction - extract triangle polygons from the edges
-void extractTriangles() {
-	std::set<Edge> edgeSet(EdgeList.begin(), EdgeList.end());
-
-	int numTriangles = 0;
-
-	for (auto it = edgeSet.begin(); it != edgeSet.end(); ++it) {
-		Edge e1 = *it;
-		auto jt = it;
-		for (++jt; jt != edgeSet.end(); ++jt) {
-			Edge e2 = *jt;
-			if (e1.intersects(e2)) {
-				Point p = e1.intersection(e2);
-				if (p != e1.p1 && p != e1.p2 && p != e2.p1 && p != e2.p2) {
-					Triangle t = { e1.p1, e1.p2, p };
-					Triangles[numTriangles] = t;
-					numTriangles++;
-					t = { e1.p1, e2.p1, p };
-					Triangles[numTriangles] = t;
-					numTriangles++;
-					t = { e1.p2, e2.p2, p };
-					Triangles[numTriangles] = t;
-					numTriangles++;
-				}
+void extractPolygons() {
+	int i = 0;
+	for (int j = 0; j < POINT_COUNT; j++) {
+		for (int k = j + 1; k < POINT_COUNT; k++) {
+			for (int l = k + 1; l < POINT_COUNT; l++) {
+				Triangle t = { P[j], P[k], P[l] };
+				Triangles[i] = t;
+				i++;
 			}
 		}
 	}
+}
 
-	printf("Triangles: %i\n", numTriangles);
+// removes triangles that overlap with other triangles
+void cleanup() {
+	std::set<Triangle> S;
+	for (Triangle& t : Triangles) {
+		if (S.find(t) == S.end()) {
+			S.insert(t);
+		}
+	}
+
+	// saves to triangles vector
+	int i = 0;
+	for (const auto& t : S) {
+		Triangles[i] = t;
+		i++;
+	}
 }
 
 // b) render the edges
@@ -318,6 +209,7 @@ void drawEdges() {
 // c) render the triangles as solid polygons
 void drawTriangles() {
 	glClear(GL_COLOR_BUFFER_BIT);
+	printf("Triangles: %d \n", Triangles.size());
 	glLineWidth(2.0f);
 	// get random color
 	glBegin(GL_TRIANGLES);
@@ -405,7 +297,7 @@ void keyboard(unsigned char key, int x, int y) {
 	case 't':
 		initPoints();
 		calcEdges();
-		extractTriangles();
+		extractPolygons();
 		drawTriangles();
 		glutPostRedisplay();
 		break;
